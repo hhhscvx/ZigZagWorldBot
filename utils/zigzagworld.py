81395c4691c6323eaf1a00e2dde176faff5e974e
hhhscvx <hhhscvx@yandex.ru>
2024-09-27 14:13:07 +0500

    Auth

--- utils/zigzagworld.py
+++ utils/zigzagworld.py
@@ -1,10 +1,17 @@
 
-from pyrogram import Client
+import asyncio
+from random import uniform
+from urllib.parse import quote, unquote
+
 import aiohttp
 from aiohttp_socks import ProxyConnector
+from pyrogram import Client
+from pyrogram.raw.functions.messages import RequestWebView
+from pyrogram.raw.types import WebViewResultUrl
 from fake_useragent import UserAgent
 
 from data import config
+from utils.core import logger
 
 
 class ZigZagWorld:
@@ -28,3 +35,45 @@ class ZigZagWorld:
 
     async def logout(self) -> None:
         await self.session.close()
+
+    async def login(self) -> bool | None:
+        query = await self.get_tg_web_data()
+
+        if query is None:
+            logger.error(f"{self.session_name} | Session {self.session_name} invalid")
+            await self.logout()
+            return None
+
+        self.session.headers['Auth'] = query
+        self.session.headers['Authorization'] = ''
+        return True
+
+    async def get_tg_web_data(self) -> str | None:
+        try:
+            await self.tg_client.connect()
+
+            await self.tg_client.send_message('ZigZagWorldBot', '/start')
+            await asyncio.sleep(uniform(1.5, 2))
+
+            web_view: WebViewResultUrl = await self.tg_client.invoke(RequestWebView(
+                peer=await self.tg_client.resolve_peer('ZigZagWorldBot'),
+                bot=await self.tg_client.resolve_peer('ZigZagWorldBot'),
+                platform='android',
+                from_bot_menu=False,
+                url="https://front.zigzagworld.online/"
+            ))
+            await self.tg_client.disconnect()
+            auth_url = web_view.url
+
+            query = unquote(string=unquote(string=auth_url.split('tgWebAppData=')[1].split('&tgWebAppVersion')[0]))
+            query_id = query.split('query_id=')[1].split('&user=')[0]
+            user = quote(query.split("&user=")[1].split('&auth_date=')[0])
+            auth_date = query.split('&auth_date=')[1].split('&hash=')[0]
+            hash_ = query.split('&hash=')[1]
+
+            print(f"query_id={query_id}&user={user}&auth_date={auth_date}&hash={hash_}")
+
+            return f"query_id={query_id}&user={user}&auth_date={auth_date}&hash={hash_}"
+
+        except Exception as err:
+            return
